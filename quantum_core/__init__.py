e7f79684342f8c5e6989587d4b578f869e378e5d
SAE8 <93454475+SAE8@users.noreply.github.com>
2025-11-29 13:47:42 +0700

    Enhance quantum_core init with engine imports
    Updated initialization to include quantum engines and solvers.

--- quantum_core/__init__.py
+++ quantum_core/__init__.py
@@ -1,2 +1,18 @@
-# Quantum Core initialization for BotZaVOD Lite version
-# (empty init file)
+# Quantum Core initialization for BotZaVOD Quantum Engine Lite
+
+from .shor_engine import ShorEngine
+from .grover_engine import GroverEngine
+from .dolce_engine import DolceEngine
+from .yota_engine import YotaEngine
+from .kva_kva_engine import KvaKvaEngine
+from .hhl_solver import HHLSolver
+
+__all__ = [
+    "ShorEngine",
+    "GroverEngine",
+    "DolceEngine",
+    "YotaEngine",
+    "KvaKvaEngine",
+    "HHLSolver",
+]
+
